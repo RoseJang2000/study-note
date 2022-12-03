4f40eb753a047a35006ebed286fb8f44709ba52f
Rose Jang <jangmi749@gmail.com>
2022-12-03 18:34:40 +0900

    JS | minigame: Basketball-game refactoring

--- basketball_game/js/script.js
+++ basketball_game/js/script.js
@@ -3,11 +3,43 @@ let userScore = 0;
 let isComputerTurn = true;
 let shotsLeft = 15;
 
+const showText = (str) => {
+    let textElem = document.getElementById('text');
+    textElem.textContent = str;
+}
+
+const updateComputerScore = (score) => {
+    comScore += score;
+
+    let comScoreElem = document.getElementById('computer-score');
+    comScoreElem.textContent = comScore;
+}
+
+const updateUserScore = (score) => {
+    userScore += score;
+
+    let userScoreElem = document.getElementById('user-score');
+    userScoreElem.textContent = userScore;
+}
+
+const disableComputerButtons = (flag) => {
+    let computerButtons = document.getElementsByClassName('btn-computer');
+
+    computerButtons[0].disabled = flag;
+}
+
+const disableUserButtons = (flag) => {
+    let userButtons = document.getElementsByClassName('btn-user');
+
+    for (let i = 0; i < userButtons.length; i++) {
+        userButtons[i].disabled = flag;
+    }
+}
+
 const onComputerShoot = () => {
     if (!isComputerTurn) return;
     else {
         let comScoreElem = document.getElementById('computer-score');
-        let textElem = document.getElementById('text');
 
         let computerButtons = document.getElementsByClassName('btn-computer');
         let userButtons = document.getElementsByClassName('btn-user');
@@ -16,27 +48,20 @@ const onComputerShoot = () => {
 
         if (shootType === 2) {
             if (Math.random() < 0.5) {
-                textElem.textContent = '컴퓨터가 2점슛을 성공시켰습니다!';
-
-                comScore += 2;
-                comScoreElem.textContent = comScore;
-            } else textElem.textContent = '컴퓨터가 2점슛을 실패했습니다.';
+                showText('컴퓨터가 2점슛을 성공시켰습니다!');
+                updateComputerScore(2);
+            } else showText('컴퓨터가 2점슛을 실패했습니다.');
         } else {
             if (Math.random() < 0.33) {
-                textElem.textContent = '컴퓨터가 3점슛을 성공시켰습니다!';
-
-                comScore += 3;
-                comScoreElem.textContent = comScore;
+                showText('컴퓨터가 3점슛을 성공시켰습니다!');
+                updateComputerScore(3);
             }
-            else textElem.textContent = '컴퓨터가 3점슛을 실패했습니다.'
+            else showText('컴퓨터가 3점슛을 실패했습니다.');
         }
 
         isComputerTurn = false;
-        
-        computerButtons[0].disabled = true;
-        for (let i = 0; i < userButtons.length; i++) {
-            userButtons[i].disabled = false;
-        }
+        disableComputerButtons(true);
+        disableUserButtons(false);
     }
 }
 
@@ -44,26 +69,21 @@ const onUserShoot = (shootType) => {
     if(isComputerTurn) return;
     else {
         let userScoreElem = document.getElementById('user-score');
-        let textElem = document.getElementById('text');
 
         let computerButtons = document.getElementsByClassName('btn-computer');
         let userButtons = document.getElementsByClassName('btn-user');
 
         if (shootType === 2) {
             if (Math.random() < 0.5) {
-                textElem.textContent = '2점슛이 성공했습니다!';
-
-                userScore += 2;
-                userScoreElem.textContent = userScore;
-            } else textElem.textContent = '2점슛이 실패했습니다.';
+                showText('2점슛이 성공했습니다!');
+                updateUserScore(2);
+            } else showText('2점슛이 실패했습니다.');
         } else {
             if (Math.random() < 0.33) {
-                textElem.textContent = '3점슛이 성공했습니다!';
-
-                userScore += 3;
-                userScoreElem.textContent = userScore;
+                showText('3점슛이 성공했습니다!');
+                updateUserScore(3);
             }
-            else textElem.textContent = '3점슛이 실패했습니다.'
+            else showText('3점슛이 실패했습니다.')
         }
 
         shotsLeft--;
@@ -72,9 +92,9 @@ const onUserShoot = (shootType) => {
         shotsLeftElem.textContent = shotsLeft;
 
         if (shotsLeft === 0) {
-            if (userScore > comScore) textElem.textContent = '승리했습니다!';
-            else if (userScore < comScore) textElem.textContent = '아쉽게도 졌습니다..';
-            else textElem.textContent = '비겼습니다.'
+            if (userScore > comScore) showText('승리했습니다!');
+            else if (userScore < comScore) showText('아쉽게도 졌습니다..');
+            else showText('비겼습니다.')
 
             computerButtons[0].disabled = true;
             for (let i = 0; i < userButtons.length; i++) {
@@ -82,11 +102,8 @@ const onUserShoot = (shootType) => {
             }
         } else {
             isComputerTurn = true;
-
-            computerButtons[0].disabled = false;
-            for (let i = 0; i < userButtons.length; i++) {
-                userButtons[i].disabled = true;
-            }
+            disableComputerButtons(false);
+            disableUserButtons(true);
         }
     }
 }
