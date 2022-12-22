e82d8fc969341d5fdfee1aeeaa37370db22f9fe9
Rose Jang <jangmi749@gmail.com>
2022-12-22 20:00:05 +0900

    JavaScript | color-flipper app

---
+++ color-flipper/app.js
@@ -0,0 +1,16 @@
+const colors = ["green", "red", "rgba(133, 122, 200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+const body = document.body;
+
+btn.addEventListener('click', () => {
+  // * get random number between 0 - 3
+  const randomNumber = getRandomNumber();
+
+  body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+})
+
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * colors.length);
+}
