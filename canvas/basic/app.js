83822cd5ae4ce72307b6c9a23d46f9007e85432a
Rose Jang <jangmi749@gmail.com>
2022-12-23 21:53:54 +0900

    JS canvas practice

---
+++ canvas/basic/app.js
@@ -0,0 +1,11 @@
+const draw = () => {
+  const canvas = document.getElementById('tutorial');
+
+  const ctx = canvas.getContext('2d');
+
+  ctx.fillStyle = 'rgb(200, 0, 0)';
+  ctx.fillRect(10, 10, 10, 10);
+
+  ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+  ctx.fillRect(30, 30, 50, 50);
+};
